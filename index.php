d11ae51f0a0cbe808bf060950e7d7e65ec49381d
Nathan Guedes <nathan.g.p@outlook.com>
2025-05-31 12:18:17 -0300

    Extracting router logic and routes into your own file.

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
+
 require_once 'functions.php';
+require_once 'router.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'],  PHP_URL_PATH);
-
-$routes = [
-    '/' => 'controllers/home.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php'
-];
-
-if(array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-}
