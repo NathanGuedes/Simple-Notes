2875602b83a1bc39eb32ef2e6be8f0d48afdc406
Nathan Guedes <nathan.g.p@outlook.com>
2025-06-03 10:51:13 -0300

    Organization of resources into appropriate directories to improve understanding of the project.

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
 return [
     '/' => 'controllers/home.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/notes/create' => 'controllers/note-create.php',
+    '/notes' => 'controllers/notes/index.php',
+    '/note' => 'controllers/notes/show.php',
+    '/notes/create' => 'controllers/notes/create.php',
 ];
